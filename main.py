fa89fd6c2c881c93396a2395a28cfde2a6a43221
paperwingedowl <noahdiengwriter@gmail.com>
2024-11-17 20:17:17 -0600

    C7: drawable/updatable groups

--- main.py
+++ main.py
@@ -14,21 +14,33 @@ def main():
     delta = 0
     refresh_rate = 120
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+
 
     while True:
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: # checks to see if you closed the window, then exits
-                return
-        
+        for u in updatable:
+            u.update(delta)
+
         screen.fill("black")
-        player.draw(screen)
-        player.update(delta)
+        
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
 
         clock.tick(refresh_rate)
         delta = clock.tick(refresh_rate) / 1000
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # checks to see if you closed the window, then exits
+                return
 
 
 if __name__ == "__main__":
